eced507f60e5c1eafb62636a7d50b045e52ac36a
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-11-30 11:50:53 +0100

    [TASK] PSR-2 of ext_localconf and ext_tables

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,81 +1,81 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Managepanels',
-	array(
-		'Panel' => 'managePanelsStartup, managePanels, new, create, edit, update, delete, duplicate, editVotings, editPanelInvitations, sendPanelInvitations, execute, votingStep',
-		'Voting' => 'listForCurrentUser, edit, update, delete, new, duplicate, sort',
-		'PanelInvitation' => 'listForCurrentUser, create, delete, getAvailablePartiesForPanel',
-		'VotingOption' => 'listForVoting, new, update,delete,sort'
+    'Visol.' . $_EXTKEY,
+    'Managepanels',
+    array(
+        'Panel' => 'managePanelsStartup, managePanels, new, create, edit, update, delete, duplicate, editVotings, editPanelInvitations, sendPanelInvitations, execute, votingStep',
+        'Voting' => 'listForCurrentUser, edit, update, delete, new, duplicate, sort',
+        'PanelInvitation' => 'listForCurrentUser, create, delete, getAvailablePartiesForPanel',
+        'VotingOption' => 'listForVoting, new, update,delete,sort'
 
-	),
-	// non-cacheable actions
-	array(
-		'Panel' => 'managePanels, new, create, edit, update, delete, duplicate, editVotings, editPanelInvitations, sendPanelInvitations, execute, votingStep',
-		'Voting' => 'listForCurrentUser, update, delete, new, duplicate, sort',
-		'PanelInvitation' => 'listForCurrentUser, create, delete, getAvailablePartiesForPanel',
-		'VotingOption' => 'listForVoting, new, update, delete, sort',
-	)
+    ),
+    // non-cacheable actions
+    array(
+        'Panel' => 'managePanels, new, create, edit, update, delete, duplicate, editVotings, editPanelInvitations, sendPanelInvitations, execute, votingStep',
+        'Voting' => 'listForCurrentUser, update, delete, new, duplicate, sort',
+        'PanelInvitation' => 'listForCurrentUser, create, delete, getAvailablePartiesForPanel',
+        'VotingOption' => 'listForVoting, new, update, delete, sort',
+    )
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Panelparticipations',
-	array(
-		'Panel' => 'panelParticipationsStartup, panelParticipations',
-		'PanelInvitation' => 'attend,ignore',
+    'Visol.' . $_EXTKEY,
+    'Panelparticipations',
+    array(
+        'Panel' => 'panelParticipationsStartup, panelParticipations',
+        'PanelInvitation' => 'attend,ignore',
 
-	),
-	// non-cacheable actions
-	array(
-		'Panel' => 'managePanels, panelParticipations',
-		'PanelInvitation' => 'attend,ignore',
-	)
+    ),
+    // non-cacheable actions
+    array(
+        'Panel' => 'managePanels, panelParticipations',
+        'PanelInvitation' => 'attend,ignore',
+    )
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Panelassignment',
-	array(
-		'PanelInvitation' => 'manageInvitations,listForPartyByDemand,assignUser,removeUser,filter',
-		'PartyMember' => 'getMembersOfCurrentParty',
+    'Visol.' . $_EXTKEY,
+    'Panelassignment',
+    array(
+        'PanelInvitation' => 'manageInvitations,listForPartyByDemand,assignUser,removeUser,filter',
+        'PartyMember' => 'getMembersOfCurrentParty',
 
-	),
-	// non-cacheable actions
-	array(
-		'PanelInvitation' => 'manageInvitations,listForPartyByDemand,assignUser,removeUser,filter',
-		'PartyMember' => 'getMembersOfCurrentParty',
-	)
+    ),
+    // non-cacheable actions
+    array(
+        'PanelInvitation' => 'manageInvitations,listForPartyByDemand,assignUser,removeUser,filter',
+        'PartyMember' => 'getMembersOfCurrentParty',
+    )
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Guestview',
-	array(
-		'Panel' => 'guestViewLogin, guestViewParticipation',
+    'Visol.' . $_EXTKEY,
+    'Guestview',
+    array(
+        'Panel' => 'guestViewLogin, guestViewParticipation',
 
-	),
-	// non-cacheable actions
-	array(
-		'Panel' => 'guestViewLogin, guestViewParticipation',
-	)
+    ),
+    // non-cacheable actions
+    array(
+        'Panel' => 'guestViewLogin, guestViewParticipation',
+    )
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Presentationview',
-	array(
-		'Panel' => 'presentationViewLogin, presentationViewParticipation',
+    'Visol.' . $_EXTKEY,
+    'Presentationview',
+    array(
+        'Panel' => 'presentationViewLogin, presentationViewParticipation',
 
-	),
-	// non-cacheable actions
-	array(
-		'Panel' => 'presentationViewParticipation',
-	)
+    ),
+    // non-cacheable actions
+    array(
+        'Panel' => 'presentationViewParticipation',
+    )
 );
 
 // Register global route
@@ -86,12 +86,12 @@ $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['easyvoteeducation'] = 'EXT:eas
 
 
 if (TYPO3_MODE == 'BE') {
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['Fab\\Vidi\\View\\MenuItem\\ExportXlsMenuItem'] = array(
-		'className' => 'Visol\\EasyvoteEducation\\Vidi\\Xclass\\View\\MenuItem\\ExportXlsMenuItem'
-	);
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['Fab\\Vidi\\View\\MenuItem\\ExportXlsMenuItem'] = array(
+        'className' => 'Visol\\EasyvoteEducation\\Vidi\\Xclass\\View\\MenuItem\\ExportXlsMenuItem'
+    );
 }
 
 /* Command Controllers */
 if (TYPO3_MODE === 'BE') {
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Visol\\EasyvoteEducation\\Command\\PanelCommandController';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Visol\\EasyvoteEducation\\Command\\PanelCommandController';
 }
--- ext_tables.php
+++ ext_tables.php
@@ -1,40 +1,40 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Managepanels',
-	'easyvote Education: Podien verwalten (für Lehrperson)'
+    $_EXTKEY,
+    'Managepanels',
+    'easyvote Education: Podien verwalten (für Lehrperson)'
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Panelparticipations',
-	'easyvote Education: Podienteilnahmen verwalten (für Politiker)'
+    $_EXTKEY,
+    'Panelparticipations',
+    'easyvote Education: Podienteilnahmen verwalten (für Politiker)'
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Panelassignment',
-	'easyvote Education: Podienteilnahmen verwalten (für Partei-Administrator)'
+    $_EXTKEY,
+    'Panelassignment',
+    'easyvote Education: Podienteilnahmen verwalten (für Partei-Administrator)'
 );
 
-$pluginSignature = str_replace('_','',$_EXTKEY) . '_panelassignment';
+$pluginSignature = str_replace('_', '', $_EXTKEY) . '_panelassignment';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForm/flexform_panelassignment.xml');
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Guestview',
-	'easyvote Education: Podiumsteilnahme als Gast'
+    $_EXTKEY,
+    'Guestview',
+    'easyvote Education: Podiumsteilnahme als Gast'
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Presentationview',
-	'easyvote Education: Präsentationsansicht (für Lehrperson)'
+    $_EXTKEY,
+    'Presentationview',
+    'easyvote Education: Präsentationsansicht (für Lehrperson)'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'easyvote Education');
@@ -51,11 +51,11 @@ $panelTable = 'tx_easyvoteeducation_domain_model_panel';
 $moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Fab\Vidi\Module\ModuleLoader', $panelTable);
 
 $moduleLoader->setIcon(sprintf('EXT:easyvote_education/Resources/Public/Icons/%s.png', $panelTable))
-	->setMainModule('easyvote')
-	->setModuleLanguageFile(sprintf('LLL:EXT:easyvote_education/Resources/Private/Language/Vidi/%s.xlf', $panelTable))
-	/*->addJavaScriptFile(sprintf('EXT:easyvote_education/Resources/Public/JavaScript/%s.js', $panelTable))*/
-	->setDefaultPid(286) // hard-coded for now
-	->register();
+    ->setMainModule('easyvote')
+    ->setModuleLanguageFile(sprintf('LLL:EXT:easyvote_education/Resources/Private/Language/Vidi/%s.xlf', $panelTable))
+    /*->addJavaScriptFile(sprintf('EXT:easyvote_education/Resources/Public/JavaScript/%s.js', $panelTable))*/
+    ->setDefaultPid(286)// hard-coded for now
+    ->register();
 
 // Backend Module for managing Frontend Users
 $usersTable = 'fe_users';
@@ -64,20 +64,20 @@ $usersTable = 'fe_users';
 $moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Fab\Vidi\Module\ModuleLoader', $usersTable);
 
 $moduleLoader->setIcon(sprintf('EXT:easyvote_education/Resources/Public/Icons/%s.png', $usersTable))
-	->setMainModule('easyvote')
-	->setModuleLanguageFile(sprintf('LLL:EXT:easyvote_education/Resources/Private/Language/Vidi/%s.xlf', $usersTable))
-	/*->addJavaScriptFile(sprintf('EXT:easyvote_education/Resources/Public/JavaScript/%s.js', $panelTable))*/
-	->setDefaultPid(144) // hard-coded for now
-	->register();
+    ->setMainModule('easyvote')
+    ->setModuleLanguageFile(sprintf('LLL:EXT:easyvote_education/Resources/Private/Language/Vidi/%s.xlf', $usersTable))
+    /*->addJavaScriptFile(sprintf('EXT:easyvote_education/Resources/Public/JavaScript/%s.js', $panelTable))*/
+    ->setDefaultPid(144)// hard-coded for now
+    ->register();
 
 
 // Signal slot to filter the Frontend Users to only display teachers and politicians
 /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
 $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
 $signalSlotDispatcher->connect(
-	'Fab\Vidi\Controller\Backend\ContentController',
-	'postProcessMatcherObject',
-	'Visol\EasyvoteEducation\Vidi\Security\FrontendUserGroupLimitationAspect',
-	'addUsergroupConstraint',
-	TRUE
+    'Fab\Vidi\Controller\Backend\ContentController',
+    'postProcessMatcherObject',
+    'Visol\EasyvoteEducation\Vidi\Security\FrontendUserGroupLimitationAspect',
+    'addUsergroupConstraint',
+    TRUE
 );
